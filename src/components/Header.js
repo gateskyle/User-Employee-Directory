efb9af11efe1ecbc7b7b2674143fcc6e2525e8f4
Kyle G <legokyle@frontier.com>
2021-04-03 19:17:37 -0700

    Inital Commit of Header file

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../styles/Header.css";
+
+function Header () {
+    return (
+      <>
+      <div className="header">
+      </div>
+      
+      <h1>Employee Directory</h1>
+      <p>Hover over the list titles and click on carrots to filter by heading or use the search box to narrow your results.</p>
+      </>
+    )
+  }
+
+export default Header;
